bb2e1883684e5ccfe65d0186d0107bf243fdf7bf
Adam Waxman <awaxman11@gmail.com>
2022-12-23 11:35:02 -0500

    Exercise 4

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -51,17 +51,24 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  padding: 6px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
-  background: var(--color-gray-300);
+  background: var(--color-gray-100);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  border-radius: 4px;
+
+  &:not(:last-of-type) {
+    margin-right: 8px;
+  }
 `;
 
 export default PhotoGridItem;
